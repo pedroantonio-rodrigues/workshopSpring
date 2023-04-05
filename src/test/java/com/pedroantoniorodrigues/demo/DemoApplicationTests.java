5c9b50b375a01aa5efc2a654ffb38191737382ed
Pedro Antonio <pedro_arxp@hotmail.com>
2023-04-05 20:19:34 -0300

    Project Created

---
+++ src/test/java/com/pedroantoniorodrigues/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroantoniorodrigues.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
